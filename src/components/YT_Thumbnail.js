b8e83249a29b59a21918df7be2a4c8008be58e36
Y1a2s3h4 <yashpurohit763@gmail.com>
2020-05-24 17:32:31 +0530

    made some changes

--- src/components/YT_Thumbnail.js
+++ src/components/YT_Thumbnail.js
@@ -1,30 +1,37 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { Input, Icon } from "react-atomize";
 
 axios.defaults.headers.get["Access-Control-Allow-Origin"] = "*";
 export default function YT_Thumbnail() {
   const [input, setInput] = useState("");
   const [data, setData] = useState([]);
   const [dataURL, setDataURL] = useState([]);
+  const [isLoading, setLoading] = useState(false);
   const ytdl_fetch = (event) => {
     event.preventDefault();
+    setLoading(true);
     axios.get(`https://apiytdl.herokuapp.com/v?input=${input}`).then((res) => {
       console.log(res);
       console.log(res.data);
       setData(res.data);
+      setLoading(false);
+
+      let len = res.data[0].thumbnail_urls.thumbnails.length - 1;
+      axios
+        .get(
+          `https://apiytdl.herokuapp.com/q?dataUrl=${res.data[0].thumbnail_urls.thumbnails[len].url}`
+        )
+        .then((res) => {
+          console.log(res);
+          console.log(res.data);
+          setDataURL(res.data);
+        });
     });
   };
 
   const onChange = (e) => {
     setInput(e.target.value);
-  };
-
-  const dataUrl = (url) => {
-    axios.get(`https://apiytdl.herokuapp.com/q?dataUrl=${url}`).then((res) => {
-      console.log(res);
-      console.log(res.data);
-      setDataURL(res.data);
-    });
   };
 
   return (
@@ -37,38 +44,56 @@ export default function YT_Thumbnail() {
           <label htmlFor="input" className="text-white">
             Enter YouTube Video Link
           </label>
-          <input
-            type="text"
-            className="form-control"
-            id="input"
-            onChange={onChange}
+          <Input
             placeholder="E.g: https://www.youtube.com/watch?v=WNWZNyXSvW0"
+            onChange={onChange}
+            suffix={
+              <Icon
+                name="Search"
+                size="20px"
+                cursor="pointer"
+                onClick={ytdl_fetch}
+                pos="absolute"
+                top="50%"
+                right="1rem"
+                transform="translateY(-50%)"
+              />
+            }
           />
         </div>
       </form>
       <div className="container">
+        {isLoading && (
+          <center>
+            <Icon name="Loading3" size="100px" />
+          </center>
+        )}
         {data.map((item, index) => {
           const url = item.thumbnail_urls.thumbnails.length - 1;
           return (
             <center key={index}>
               {" "}
               <br />
-              <div className="card shadow" style={{ width: "55rem" }}>
+              <div className="card shadow" style={{ maxWidth: "55rem" }}>
                 <img
                   src={item.thumbnail_urls.thumbnails[url].url}
                   id="img"
-                  className="card-img-top"
+                  className="img-fluid"
                   alt="..."
                 />
                 <a
-                  onClick={() => {
-                    dataUrl(item.thumbnail_urls.thumbnails[url].url);
-                  }}
                   href={dataURL.dataURL}
-                  target="_blank"
                   download="thumbnail.jpeg"
+                  className="download"
                 >
-                  Download &darr;
+                  {" "}
+                  Download{" "}
+                  <Icon
+                    name="Download"
+                    size="16px"
+                    m={{ b: "5px" }}
+                    color="white"
+                  />
                 </a>
               </div>
             </center>
